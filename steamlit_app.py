2f605831c68f38459d1b2ef9e95aa86a5952bf2c
UserBD38000 <151024601+UserBD38000@users.noreply.github.com>
2024-01-17 19:33:52 +0100

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -18,8 +18,15 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-streamlit.datafraùe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
