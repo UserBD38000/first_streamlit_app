dad7715ef5125ff183344209d451658e41909da9
UserBD38000 <151024601+UserBD38000@users.noreply.github.com>
2024-01-17 19:13:11 +0100

    Update steamlit_app.py
    added the import snowflake

--- steamlit_app.py
+++ steamlit_app.py
@@ -21,3 +21,5 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.datafraùe(my_fruit_list)
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+import snowflake.connector
